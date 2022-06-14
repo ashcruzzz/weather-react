78fc88836f03a0a64a591bab349312121a7bcb9c
ashcruzzz <ashleycruzart@gmail.com>
2022-06-14 00:45:36 -0700

    open source link

--- src/BottomHeader.js
+++ src/BottomHeader.js
@@ -3,10 +3,7 @@ import React from "react";
 export default function BottomHeader() {
   return (
     <div id="bottom-header">
-      <a
-        href="https://github.com/ashcruzzz/ash-cruz-project-app"
-        id="open-source"
-      >
+      <a href="https://github.com/ashcruzzz/weather-react" id="open-source">
         Open-source code
       </a>
       <span> by Ashley Cruz</span>
