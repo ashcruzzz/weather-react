e6b163490434a93a6563a7b2f496a2735363c583
ashcruzzz <ashleycruzart@gmail.com>
2022-06-23 15:30:42 -0700

    started build

--- src/MainInfoBlock.js
+++ src/MainInfoBlock.js
@@ -1,6 +1,7 @@
 import React from "react";
 import LeftBlock from "./LeftBlock";
 import RightBlock from "./RightBlock";
+
 export default function MainInfoBlock() {
   return (
     <div className="row" id="main-info-block">
